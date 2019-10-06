451e6facb81766aa02dab68c9c6daa19b09ebc6c
Alex B. Santos <alexbenincasa@ymail.com>
2019-10-06 03:14:33 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # tcerj
+
+Extração de dados com Selenium
+
+## Instalação
+
+```
+pip install selenium
+```
+
+## Modo de usar
+
+Procure o path do python instalado no Windows:
+
+```
+/mnt/c/Users/alexb/AppData/Local/Programs/Python/Python37-32/python.exe contasIrregulares.py
+```
+
+## Resultado
+
+Resultados encontrados 2678 / Atualizado em 02/10/2018 
+
+| Processo | Nome | Cpf | Data do Trânsito em Julgado |
+| 200335-1/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 27/01/2015 |
+| 226866-7/2010 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 22/04/2015 |
+| 230321-6/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 22/07/2015 |
+| 230327-0/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 22/10/2014 |
+| 230334-3/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 21/10/2015 |
---
+++ contasIrregulares.py
@@ -0,0 +1,29 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By as by
+from selenium.webdriver.support.ui import WebDriverWait
+
+# caminho para o binário do geckodriver.exe
+bin_path = 'C:\\www\\tcerj\\geckodriver.exe'
+
+browser = webdriver.Firefox(executable_path=bin_path)
+wait = WebDriverWait(browser, 15)
+
+url = "https://www.tcerj.tc.br/contas-irregulares/#/home"
+browser.get(url)
+
+# esperando os dados serem carregados
+wait.until(ec.presence_of_all_elements_located((by.CSS_SELECTOR, '#datagrid-0 tbody tr')))
+
+while True:
+	for row in browser.find_elements_by_css_selector('#datagrid-0 tbody tr'):
+		print(row.text)
+
+	if browser.find_element_by_css_selector('li.pagination-next'):
+		browser.find_element_by_css_selector('li.pagination-next a').click()
+	else:
+		break
+
+browser.quit()
