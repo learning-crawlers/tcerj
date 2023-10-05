97ed6d9437bda527da64e9c88a2ee467bb3acb5a
Julytics <juliannafabiola06@gmail.com>
2023-10-05 10:08:03 -0300

    atualizando e melhorando o script

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ Procure o path do python instalado no Windows:
 
 Resultados encontrados 2678 / Atualizado em 02/10/2018 
 
-| Processo | Nome | Cpf | Data do Trânsito em Julgado |
-| --- | --- | --- | --- |
-| 200335-1/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 27/01/2015 |
-| 226866-7/2010 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 22/04/2015 |
-| 230321-6/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 22/07/2015 |
-| 230327-0/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 22/10/2014 |
-| 230334-3/2006 | AARÃO DE MOURA BRITO NETO | 582708767-04 | 21/10/2015 |
+| Processo | Nome | Cpf | Data do Trânsito em Julgado | Data da Irregularidade |
+| --- | --- | --- | --- | --- |
+| 200335-1/2006 | AARÃO DE MOURA BRITO NETO | 582.708.***-04 | 27/01/2015 | 25/11/2014 |
+| 226866-7/2010 | AARÃO DE MOURA BRITO NETO | 582.708.***-04 | 22/04/2015 | 19/08/2014 |
+| 230321-6/2006 | AARÃO DE MOURA BRITO NETO | 582.708.***-04 | 22/07/2015 | 22/05/2014 |
+| 230327-0/2006 | AARÃO DE MOURA BRITO NETO | 582.708.***-04 | 22/10/2014 | 04/09/2014 |
+| 230334-3/2006 | AARÃO DE MOURA BRITO NETO | 582.708.***-04 | 21/10/2015 | 27/08/2015 |
--- contasIrregulares.py
+++ contasIrregulares.py
@@ -1,38 +1,16 @@
-#######################################
-##  TCERJ Contas Irregulares
-##    Busca de contas irregulares no TCERJ com Número de Processo, Nome, CPF e Data do Trânsito em Julgado
-##
-##    Author: Alex Benincasa Santos 
-##    Mail: alexbenincasa@ymail.com
-##    2019
-#######################################
 
-import os
-import time
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.by import By as by
-from selenium.webdriver.support.ui import WebDriverWait
 
-# caminho para o binário do geckodriver.exe
-bin_path = 'C:\\www\\tcerj\\geckodriver.exe'
+def requisitar_site(url):
+    pass
 
-browser = webdriver.Firefox(executable_path=bin_path)
-wait = WebDriverWait(browser, 15)
+def analisar_requisicao(content):
+    pass
 
-url = "https://www.tcerj.tc.br/contas-irregulares/#/home"
-browser.get(url)
+def tratamento_dados(raw_data):
+    pass
 
-# esperando os dados serem carregados
-wait.until(ec.presence_of_all_elements_located((by.CSS_SELECTOR, '#datagrid-0 tbody tr')))
+def armazenar_dados(trusted_data):
+    pass
 
-while True:
-	for row in browser.find_elements_by_css_selector('#datagrid-0 tbody tr'):
-		print(row.text)
-
-	if browser.find_element_by_css_selector('li.pagination-next'):
-		browser.find_element_by_css_selector('li.pagination-next a').click()
-	else:
-		break
-
-browser.quit()
+def inicio_crawler(contador):
+    pass
